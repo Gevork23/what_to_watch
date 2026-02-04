e2e7b16e2146207d6f5e1e2b39fddf46022117d9
Gevork23 <gevork230702@mail.ru>
2026-02-04 20:14:50 +0300

    1

--- opinions_app.py
+++ opinions_app.py
@@ -3,36 +3,21 @@
 from datetime import datetime
 from random import randrange
 
-# Импортировать функцию render_template().
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
 
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SECRET_KEY'] = 'MYSECRETKEY'
+app.config['SECRET_KEY'] = 'MY SECRET KEY'
 
 db = SQLAlchemy(app)
 
-class OpinionForm(FlaskForm):
-    title = StringField(
-        'Введите название фильма',
-        validators=[DataRequired(message='Обязательное поле'),
-                    Length(1, 128)]
-    )
-    text = TextAreaField(
-        'Напишите мнение', 
-        validators=[DataRequired(message='Обязательное поле')]
-    )
-    source = URLField(
-        'Добавьте ссылку на подробный обзор фильма',
-        validators=[Length(1, 256), Optional()]
-    )
-    submit = SubmitField('Добавить')
 
 class Opinion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,6 +26,23 @@ class Opinion(db.Model):
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
+
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'), Length(1, 128)],
+    )
+    text = TextAreaField(
+        'Напишите мнение',
+        validators=[DataRequired(message='Обязательное поле')],
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()],
+    )
+    submit = SubmitField('Добавить')
+
+
 @app.route('/')
 def index_view():
     quantity = Opinion.query.count()
@@ -48,37 +50,36 @@ def index_view():
         return 'В базе данных записей нет.'
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
-    # Подключить шаблон opinion.html.
     return render_template('opinion.html', opinion=opinion)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
     form = OpinionForm()
-    # Если ошибок не возникло...
     if form.validate_on_submit():
-        # ...то нужно создать новый экземпляр класса Opinion...
+        text = form.text.data
+        # Если в БД уже есть мнение с текстом, который ввёл пользователь...
+        if Opinion.query.filter_by(text=text).first() is not None:
+            # ...вызвать функцию flash и передать соответствующее сообщение.
+            flash('Такое мнение уже было оставлено ранее!')
+            # Вернуть пользователя на страницу «Добавить новое мнение».
+            return render_template('add_opinion.html', form=form)
         opinion = Opinion(
-            # ...и передать в него данные, полученные из формы.
-            title=form.title.data,
-            text=form.text.data,
+            title=form.title.data, 
+            text=text, 
             source=form.source.data
         )
-        # Затем добавить его в сессию работы с базой данных...
         db.session.add(opinion)
-        # ...и зафиксировать изменения.
         db.session.commit()
-        # Затем переадресовать пользователя на страницу добавленного мнения.
         return redirect(url_for('opinion_view', id=opinion.id))
-    # Если валидация не пройдена - просто отрисовать страницу с формой.
-    return render_template('add_opinion.html', form=form) 
+    return render_template('add_opinion.html', form=form)
 
-@app.route('/opinions/<int:id>')  
-# Параметром указывается имя переменной.
-def opinion_view(id):  
-    # Теперь можно запросить нужный объект по id...
-    opinion = Opinion.query.get_or_404(id)  
-    # ...и передать его в шаблон (шаблон - тот же, что и для главной страницы).
+
+@app.route('/opinions/<int:id>')
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
+
 
 if __name__ == '__main__':
     app.run()
